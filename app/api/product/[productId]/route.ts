fa337fea4b41fea325e8e0309855aede043b1508
Millenniumist <suparit.panta@gmail.com>
2024-10-24 14:30:06 +0700

    fix delete

--- app/api/product/[productId]/route.ts
+++ app/api/product/[productId]/route.ts
@@ -1,7 +1,11 @@
 import { NextResponse } from 'next/server';
 import prisma from '@/config/prisma';
 import { v2 as cloudinary } from 'cloudinary';
-
+cloudinary.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET
+});
 export async function GET(
   request: Request,
   { params }: { params: { productId: number } }
@@ -51,21 +55,20 @@ export async function POST(req: Request) {
   }
 }
 
-export async function DELETE(req: Request, { params }: { params: { productId: number } }) {
+export async function DELETE(
+  req: Request,
+  { params }: { params: { productId: string } }
+) {
   try {
-    // Fetch the product with its images
-    const productId = Number(params.productId)
-    console.log(params.productId)
+    const productId = Number(params.productId);
+    
     const product = await prisma.product.findUnique({
       where: { id: productId },
       include: { images: true },
     });
 
     if (!product) {
-      return new Response(JSON.stringify({ error: 'Product not found' }), {
-        status: 404,
-        headers: { 'Content-Type': 'application/json' },
-      });
+      return NextResponse.json({ error: 'Product not found' }, { status: 404 });
     }
 
     // Delete images from Cloudinary
@@ -78,19 +81,13 @@ export async function DELETE(req: Request, { params }: { params: { productId: nu
 
     // Delete the product and its associated images from the database
     const deletedProduct = await prisma.product.delete({
-      where: { id: Number(productId) },
+      where: { id: productId },
       include: { images: true },
     });
 
-    return new Response(JSON.stringify({ deletedProduct }), {
-      status: 200,
-      headers: { 'Content-Type': 'application/json' },
-    });
+    return NextResponse.json({ deletedProduct });
   } catch (error) {
-    console.log(error)
-    return new Response(JSON.stringify({ error: 'Failed to delete product' }), {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' },
-    })
+    console.log(error);
+    return NextResponse.json({ error: 'Failed to delete product' }, { status: 500 });
   }
 }
